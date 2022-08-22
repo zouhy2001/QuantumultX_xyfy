56962948c8a2eeb55d970c29dc91f528fe5b434c
zouhy2001 <57806936+zouhy2001@users.noreply.github.com>
2022-08-22 21:19:02 +0800

    Update xyfy.token.js

--- xyfy.token.js
+++ xyfy.token.js
@@ -3,7 +3,7 @@ const tokenKey = 'token_xyfy'
 const chavy = init()
 const auth_code = $request.body
 //console.log(auth_code);
-if (auth_code != chavy.getdata(tokenKey && auth_code)) {
+if (auth_code != chavy.getdata(tokenKey) && auth_code) {
   if (chavy.setdata(tokenKey, auth_code)) {
     chavy.msg(`${tokenName}`, '获取auth_code: 成功', '更新时间 '+new Date().toLocaleTimeString(‘cn’,{hour12:false}))
     chavy.log(`[${tokenName}] 获取auth_code: 成功, auth_code: ${auth_code}`)
